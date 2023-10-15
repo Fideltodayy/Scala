156770b0dabd2f8ed3adbb9590aba8de3a2abe92
Fidel Otieno <fidelotieno11@gmail.com>
2023-10-15 12:47:44 +0300

    Added .gitignore file to ignore the history folder

--- ClassesnObjects/basicConstructor/basicConstructor.scala
+++ ClassesnObjects/basicConstructor/basicConstructor.scala
@@ -2,10 +2,10 @@ class Person(var firstName: String, var lastName: String);
 
 object basicConstructor {
     def main(args: Array[String]) ={
-        val p = new Person("Bill", "Panner");
+        val p = new Person("Fidel", "Otieno");
         println(p.firstName + " " + p.lastName);
-        p.firstName = "William";
-        p.lastName = "Bernheim";
+        p.firstName = "Fidel";
+        p.lastName = "Toadyy";
         println(p.firstName + " " + p.lastName);
 
     }
