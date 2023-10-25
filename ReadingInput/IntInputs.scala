5eac68f1e6392a28702795b522ac3d411ead507f
Fidel Otieno <fidelotieno11@gmail.com>
2023-10-25 05:02:31 +0300

    Understood teh readInt function from scala.i.o.StdIn

--- ReadingInput/IntInputs.scala
+++ ReadingInput/IntInputs.scala
@@ -7,8 +7,8 @@ object IntInputs extends App {
 }
 
 //A function to read from the keyboard
-def totalMarks1(a: Double = readInt(),b:Double = readInt(),c: Double = readInt()) : Double = a+b+c;
+def totalMarks1(a: Int = readInt(),b:Int = readInt(),c: Int = readInt()) = a+b+c;
 
 // A function to generate the marks randomly
 
-def totalMarks(a: Double = (Math.ceil(Math.random()* 10 )), b:Double = (Math.ceil(Math.random()*20)), c:Double = (Math.ceil(Math.random()* 70 )) ) : Double = a + b + c;
+// def totalMarks(a: Int = (Math.ceil(Math.random()* 10 )), b:Int = (Math.ceil(Math.random()*20)), c:Int = (Math.ceil(Math.random()* 70 )) ) : Int = a + b + c;
